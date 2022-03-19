0f7ae6692e7322e7746c6427f43827c5cdd0e986
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-03-19 14:22:26 +0300

    Add AgentInstance type

--- src/services/api/api.types.ts
+++ src/services/api/api.types.ts
@@ -1,8 +1,30 @@
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
+
 export type Coin = {
   id: string;
   symbol: string;
   amount: number;
 };
 export type CoinsResponse = Coin[];
-export type AddCoinPayload = Omit<Coin, 'symbol'>;
+export type AddCoinPayload = Coin;
 export type AddCoinResponse = Coin;
+
+type AgentInstanceOriginal = Omit<
+  AxiosInstance,
+  'get' | 'delete' | 'post' | 'put'
+>;
+
+export interface AgentInstance extends AgentInstanceOriginal {
+  get: <T = any>(url: string, config?: AxiosRequestConfig) => Promise<T>;
+  delete: <T = any>(url: string, config?: AxiosRequestConfig) => Promise<T>;
+  post: <T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ) => Promise<T>;
+  put: <T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ) => Promise<T>;
+}
