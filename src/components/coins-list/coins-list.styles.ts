a75216a50b6fc0016654d2671eeaea42b78ecbbb
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-03-19 19:36:01 +0300

    Add coin list component

---
+++ src/components/coins-list/coins-list.styles.ts
@@ -0,0 +1,8 @@
+import { makeStyles, Theme } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.paper,
+    marginTop: 80,
+  },
+}));
---
+++ src/components/coins-list/coins-list.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Coin } from '@/services/api/api.types';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+
+import { useStyles } from './coins-list.styles';
+import CoinItem from '@/components/coin-item/coin-item';
+
+const data: Coin[] = [
+  {
+    id: 'ethereum',
+    symbol: 'eth',
+    amount: 2,
+  },
+  {
+    id: 'bitcoin',
+    symbol: 'btc',
+    amount: 2,
+  },
+  {
+    id: 'string',
+    symbol: 'string',
+    amount: 0,
+  },
+];
+
+const CoinsList: React.FC = () => {
+  const styles = useStyles();
+
+  return (
+    <div className={styles.container}>
+      <TableContainer component={Paper}>
+        <Table aria-label={'simple table'}>
+          <TableHead>
+            <TableRow>
+              <TableCell>{'Token'}</TableCell>
+              <TableCell align={'right'}>{'Quantity'}</TableCell>
+              <TableCell align={'right'}>{'Amount in USD'}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((coin, index) => (
+              <CoinItem coin={coin} key={`${coin.id}-${index}`} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default CoinsList;
