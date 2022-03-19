e3ee8c0d5356ee4efe393c2501f6fce1586099e2
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-03-19 11:43:09 +0300

    Add base jest configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/**/index.tsx',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+}
---
+++ src/app.spec.ts
@@ -0,0 +1,5 @@
+describe('Test', () => {
+  it('should test', function () {
+    expect(true).toBe(true);
+  });
+});
