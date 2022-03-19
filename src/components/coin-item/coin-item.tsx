bc2b8f6b6c63c37a50203d5c3c769325bc3c7646
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-03-19 21:58:55 +0300

    Add coin form to coin item

--- src/components/coin-item/coin-item.tsx
+++ src/components/coin-item/coin-item.tsx
@@ -1,50 +1,32 @@
-import React, { useState } from 'react';
-import { IconButton, TableCell, TableRow, TextField } from '@material-ui/core';
+import React from 'react';
+import { IconButton, TableCell, TableRow } from '@material-ui/core';
 import { Coin } from '@/services/api/api.types';
 import EditIcon from '@material-ui/icons/Edit';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-import SaveIcon from '@material-ui/icons/Save';
+import { useModal } from '@/hooks/useModal';
+import CoinForm from '@/components/coin-form';
 
 type CoinItemProps = {
   coin: Coin;
 };
 
 const CoinItem: React.FC<CoinItemProps> = ({ coin }) => {
-  const [isEditing, setIsEditing] = useState(false);
+  const { show, hide, RenderModal } = useModal();
 
   const { symbol, amount } = coin;
 
   const onDelete = (): void => console.log('deleted');
-  const onEdit = (): void => setIsEditing(true);
-  const onSave = (): void => {
-    setIsEditing(false);
-  };
 
   return (
     <TableRow>
       <TableCell component={'th'} scope={'row'}>
         {symbol.toUpperCase()}
       </TableCell>
-      <TableCell align={'right'} style={{ width: 100 }}>
-        {isEditing ? (
-          <TextField
-            id={'outlined-basic'}
-            value={amount}
-            variant={'outlined'}
-            size={'small'}
-          />
-        ) : (
-          amount
-        )}
-      </TableCell>
+      <TableCell align={'right'}>{amount}</TableCell>
       <TableCell align={'right'}>{'--'}</TableCell>
       <TableCell align={'right'}>
-        <IconButton
-          onClick={isEditing ? onSave : onEdit}
-          edge={'end'}
-          aria-label={'update'}
-        >
-          {isEditing ? <SaveIcon /> : <EditIcon />}
+        <IconButton onClick={show} edge={'end'} aria-label={'update'}>
+          <EditIcon />
         </IconButton>
       </TableCell>
       <TableCell align={'center'}>
@@ -52,6 +34,9 @@ const CoinItem: React.FC<CoinItemProps> = ({ coin }) => {
           <HighlightOffIcon />
         </IconButton>
       </TableCell>
+      <RenderModal>
+        <CoinForm title={symbol} initialAmount={amount} onCancel={hide} />
+      </RenderModal>
     </TableRow>
   );
 };
