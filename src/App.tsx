60d51bfb00b96ea5e6a21420e6741775028105d0
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-03-22 23:24:52 +0300

    Add toast component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
-import CoinsList from '@/components/coins-list/coins-list';
+import CoinsList from '@/components/coins-list';
+import ToastMessage from '@/components/toast-message';
 
 const App: React.FC = () => (
   <>
@@ -14,6 +15,7 @@ const App: React.FC = () => (
     <Container maxWidth={'lg'}>
       <CoinsList />
     </Container>
+    <ToastMessage />
   </>
 );
 
