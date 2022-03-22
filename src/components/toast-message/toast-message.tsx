ca83d9b789d2eb817b573e0f329ceed9d77830bc
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-03-22 23:24:25 +0300

    Add toast message component

---
+++ src/components/toast-message/toast-message.tsx
@@ -0,0 +1,82 @@
+import React, { useContext, useEffect, useMemo } from 'react';
+import { observer } from 'mobx-react-lite';
+import MessageStore, { MessageType } from '@/store/MessageStore';
+import {
+  IconButton,
+  Snackbar,
+  SnackbarContent,
+  useTheme,
+} from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
+const ToastMessage: React.FC = observer(() => {
+  const theme = useTheme();
+  const { hasMessage, message, removeMessage, clear } =
+    useContext(MessageStore);
+
+  const { text = '', type } = message ?? {};
+
+  useEffect(() => clear, []);
+
+  const snackBackground = useMemo(() => {
+    const bg = { background: theme.palette.background.paper };
+
+    switch (type) {
+      case MessageType.SUCCESS:
+        bg.background = theme.palette.success.main;
+        break;
+      case MessageType.WARN:
+        bg.background = theme.palette.warning.main;
+        break;
+      case MessageType.ERROR:
+        bg.background = theme.palette.error.main;
+        break;
+      default:
+        break;
+    }
+
+    return bg;
+  }, [type]);
+
+  const SnackbarMessage = (
+    <SnackbarContent
+      aria-describedby={'message-id2'}
+      style={snackBackground}
+      message={
+        <span id={'message-id2'}>
+          <div>{text}</div>
+        </span>
+      }
+      action={
+        <IconButton
+          size={'small'}
+          aria-label={'close'}
+          color={'inherit'}
+          onClick={removeMessage}
+        >
+          <CloseIcon fontSize={'small'} />
+        </IconButton>
+      }
+    />
+  );
+
+  const EmptySnackMessage = (
+    <SnackbarContent
+      aria-describedby={'message-id2'}
+      style={snackBackground}
+      message={''}
+    />
+  );
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      open={hasMessage}
+      onClose={removeMessage}
+    >
+      {hasMessage ? SnackbarMessage : EmptySnackMessage}
+    </Snackbar>
+  );
+});
+
+export default ToastMessage;
